aa035890ca054e900e5eef2d2b8490826d5493d7
Koh Yi Da <kohyida1997@gmail.com>
2021-12-28 15:49:06 +0000

    Add .gitignore

--- GenerateData.sh
+++ GenerateData.sh
@@ -1,7 +1,7 @@
 
 #echo Generating 16GB of Records, 32B per Record, SEED = 1234
-#./GenerateData 536870912 1234 # 32 bytes Record, 16GB total size. 2^29 Records
+#./GenerateData.o 536870912 1234 # 32 bytes Record, 16GB total size. 2^29 Records
 
 echo Generating 1GB of Records, 32B per Record, SEED = 1234
 echo
-./GenerateData 33554432 1234 # 32 bytes Record, 1GB total size. 2^25 Records, SEED = 1234
+./GenerateData.o 33554432 1234 # 32 bytes Record, 1GB total size. 2^25 Records, SEED = 1234
--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 build_all:
-	g++ -std=c++17 -o GenerateData GenerateData.cpp -fopenmp -lpthread -lpmem
+	g++ -std=c++17 -o GenerateData.o GenerateData.cpp -fopenmp -lpthread -lpmem
